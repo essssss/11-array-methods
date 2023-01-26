920c2d6bfa4066fdb1b72de93464c0fa6c745119
essssss <srmcnamara7@gmail.com>
2023-01-26 14:27:16 -0500

    added more lessons. Final forEach lesson

--- forEach/app.js
+++ forEach/app.js
@@ -39,17 +39,26 @@
 
 const colors = ["teal", "cyan", "peach", "purple"];
 
-function yell(val, i) {
-  const caps = val.toUpperCase();
-  console.log(`At index ${i}, ${caps}`);
+// function yell(val, i) {
+//   const caps = val.toUpperCase();
+//   console.log(`At index ${i}, ${caps}`);
+// }
+
+// colors.forEach(yell);
+
+// const prices = [30.99, 19.99, 2.5, 99.0];
+
+// let total = 0;
+// prices.forEach(function (price) {
+//   total += price;
+// });
+// console.log(total);
+function forEach(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    callback(arr[i], i, arr);
+  }
 }
 
-colors.forEach(yell);
-
-const prices = [30.99, 19.99, 2.5, 99.0];
-
-let total = 0;
-prices.forEach(function (price) {
-  total += price;
+forEach(colors, function (color, i) {
+  console.log(color.toUpperCase(), "at index of", i);
 });
-console.log(total);

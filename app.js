1d4c185d91a96f29d39ee20209dc7ffe232460d2
essssss <srmcnamara7@gmail.com>
2023-01-26 14:18:43 -0500

    forEach lesson

--- app.js
+++ app.js
@@ -1,38 +1,55 @@
-function holler() {
-  console.log("HEY YOU");
+// function holler() {
+//   console.log("HEY YOU");
+// }
+
+// const whisper = function () {
+//   console.log("psst...i have a secret");
+// };
+
+// function add(x, y) {
+//   return x + y;
+// }
+
+// function subtract(x, y) {
+//   return x - y;
+// }
+
+// function multiply(x, y) {
+//   return x * y;
+// }
+
+// function divide(x, y) {
+//   return x / y;
+// }
+
+// const mathFuncs = [add, subtract, multiply, divide];
+
+// // setTimeout(whisper, 4000);
+
+// function doMath(a, b, mathFunc) {
+//   mathFunc(a, b);
+//   return mathFunc(a, b);
+// }
+
+// function doAllMath(a, b, mathFuncs) {
+//   for (let func of mathFuncs) {
+//     console.log(func(a, b));
+//   }
+// }
+
+const colors = ["teal", "cyan", "peach", "purple"];
+
+function yell(val, i) {
+  const caps = val.toUpperCase();
+  console.log(`At index ${i}, ${caps}`);
 }
 
-const whisper = function () {
-  console.log("psst...i have a secret");
-};
+colors.forEach(yell);
 
-function add(x, y) {
-  return x + y;
-}
+const prices = [30.99, 19.99, 2.5, 99.0];
 
-function subtract(x, y) {
-  return x - y;
-}
-
-function multiply(x, y) {
-  return x * y;
-}
-
-function divide(x, y) {
-  return x / y;
-}
-
-const mathFuncs = [add, subtract, multiply, divide];
-
-// setTimeout(whisper, 4000);
-
-function doMath(a, b, mathFunc) {
-  mathFunc(a, b);
-  return mathFunc(a, b);
-}
-
-function doAllMath(a, b, mathFuncs) {
-  for (let func of mathFuncs) {
-    console.log(func(a, b));
-  }
-}
+let total = 0;
+prices.forEach(function (price) {
+  total += price;
+});
+console.log(total);
